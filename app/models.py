2aa9b2ac8af6cf14a8c81a4c2e9562c2231ae243
kahenya-anita <anitakahenya1@gmail.com>
2020-10-05 00:17:11 +0300

    Add user class

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
-#from . import db
+from . import db
 
 #from . import login_manager
 
@@ -17,28 +17,28 @@ class Quote:
         self.quote = quote
         self.permalink = permalink
 
-# class User(UserMixin, db.Model):
-#     __tablename__ = 'users'
-#     # id = db.Column(db.Integer,primary_key = True)
-#     # username = db.Column(db.String(255),index = True)
-#     # email = db.Column(db.String(255),unique = True,index = True)    
-#     # password_hash = db.Column(db.String(255))
-#     # name = db.Column(db.String)
-#     # prof_pic_path = db.Column(db.String)
-#     # bio = db.Column(db.String)
-#     # password_secure = db.Column(db.String(255))
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)    
+    password_hash = db.Column(db.String(255))
+    name = db.Column(db.String)
+    prof_pic_path = db.Column(db.String)
+    bio = db.Column(db.String)
+    password_secure = db.Column(db.String(255))
 
-#     @property
-#     def password(self):
-#         raise AttributeError('You cannot read the password attribute')
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-#     @password.setter
-#     def password(self, password):
-#         self.password_hash = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
 
-#     def verify_password(self,password):
-#         return check_password_hash(self.password_hash,password)
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
-#     def __repr__(self):
-#         return f'{self.username}'
+    def __repr__(self):
+        return f'{self.username}'
