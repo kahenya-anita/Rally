500cafc161d5ff8f2ee292ebaa9115bfbb387acf
kahenya-anita <anitakahenya1@gmail.com>
2020-10-04 16:58:59 +0300

    Add forms

---
+++ app/main/views.py
@@ -0,0 +1,41 @@
+from flask import render_template
+from app import app
+
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    quote = get_quote()
+    author = User.query.first() 
+    return render_template('index.html',quote=quote, author=author)
+
+@main.route('/user',methods = ['GET','POST'])
+def user(): 
+    form=PostForm()
+    quote=get_quote()        
+    if form.validate_on_submit():
+        title = form.title.data
+        text = form.text.data
+        
+        
+        # Updated post instance
+        this_post = Post(title=title, text=text,  post_pic_path='photos/sample-image.jpeg')
+
+        # save post method
+        this_post.save_post()
+
+        mail_list=[]
+        subscribers=Subscriber.query.order_by(Subscriber.id.desc())
+        for sub in subscribers:
+            mail_list.append(sub.email)
+        
+        mail_message("New posts on Blog post","email/new_post",mail_list,this_post=this_post)
+
+        return redirect(url_for('.index'))
+      
+    title = 'New Post'
+
+    return render_template('user.html', title=title, post_form=form, quote=quote)
+
