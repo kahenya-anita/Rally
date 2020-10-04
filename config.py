482bd60273717dd0a85fd6c00d8c32c14024e9be
kahenya-anita <anitakahenya1@gmail.com>
2020-10-05 00:07:42 +0300

    Initialize db

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     QUOTE_API_KEY = os.environ.get('QUOTE_API_KEY')
     SECRET_KEY = ('e1cb1d88ff8e0b')
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://yunidfbnwrdsxo:688063753c46cc99c32a873c4ebf92117fd5f70aaf7f3286fb278904bf4bd891@ec2-52-87-22-151.compute-1.amazonaws.com:5432/ddnsjnb7s86ukt'
 
 
 
