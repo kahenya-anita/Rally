e05795578ebfab454700b45cab2ad2a07514be36
kahenya-anita <anitakahenya1@gmail.com>
2020-10-05 01:35:05 +0300

    Add post views

--- app/main/views.py
+++ app/main/views.py
@@ -35,11 +35,59 @@ def user():
         for sub in subscribers:
             mail_list.append(sub.email)
         
-        mail_message("New posts on Blog post","email/new_post",mail_list,this_post=this_post)
+        mail_message("New posts on Rally","email/new_post",mail_list,this_post=this_post)
 
         return redirect(url_for('.index'))
       
     title = 'New Post'
 
     return render_template('user.html', title=title, post_form=form, quote=quote)
+
+@main.route('/post/<post_id>',methods = ['GET','POST'])
+def blog_post(post_id):
+    quote=get_quote()
+    post=Post.query.filter_by(id=post_id).first()
+    form = CommentForm()
+      
+    if form.validate_on_submit():
+        new_comment = form.comment.data             
+
+        # Updated comment instance
+        this_comment = Comment(comment_text=new_comment,post=post)
+
+        # save comment method
+        this_comment.save_comment()
+        return redirect(url_for('.blog_post', post_id=post_id))
+    
+    
+    comments=Comment.query.filter_by(post_id=post_id).order_by(Comment.posted.desc())  
+    title = post.title
+    return render_template('post.html',title = title, comments=comments, comment_form=form, post=post, quote=quote)
+    
+@main.route('/post/update/<post_id>',methods = ['GET','POST'])
+@login_required
+def update_blog(post_id):
+    form=PostForm()
+    post = Post.query.filter_by(id = post_id).first()
+    if post is None:
+        abort(404)       
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.text = form.text.data       
+
+        db.session.add(post)
+        db.session.commit()
+       
+        return redirect(url_for('.blog_post', post_id=post_id))
+    
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        post.post_pic_path = path
+        db.session.commit()
+        return redirect(url_for('.blog_post', post_id=post_id))
+      
+    title = 'Update post'
+
+    return render_template('update_post.html', title=title, post_form=form, post=post)  
 
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
-from flask_script import Manager,Server
-from app.models import User
+from flask_script import Manager, Server
+from app.models import User, Post
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
@@ -14,9 +14,9 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db, User = User, Post = Post )
 
-    
+
 @manager.command
 def test():
     """Run the unit tests."""
