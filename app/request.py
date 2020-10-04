20f4ceaf113c8e1d2af9fbb1c62fa5f7c7b33b83
kahenya-anita <anitakahenya1@gmail.com>
2020-10-04 16:50:03 +0300

    Make an API call

---
+++ app/request.py
@@ -0,0 +1,26 @@
+import urllib.request,json
+from .models import Quote
+
+# Getting api key
+base_url = None
+
+def configure_request(app):
+
+    global base_url    
+    base_url = app.config['QUOTE_API_BASE_URL']
+
+def get_quote():
+    
+    get_quote_url = base_url
+
+    with urllib.request.urlopen(get_quote_url) as url:
+        get_quote_data = url.read()
+        get_quote_response = json.loads(get_quote_data)
+        
+        author = get_quote_response.get('author')
+        quote = get_quote_response.get('quote')
+        permalink = get_quote_response.get('permalink')
+
+        quote_object = Quote(author,quote,permalink)
+        
+    return quote_object
--- app/views.py
+++ app/views.py
@@ -7,6 +7,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    quote = get_quote()
+    author = User.query.first() 
+    return render_template('index.html',quote=quote, author=author)
