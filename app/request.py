efdbce6e6382b60a0fd2f09c272d928e6456e837
kahenya-anita <anitakahenya1@gmail.com>
2020-10-07 10:28:34 +0300

    Migrations

--- app/request.py
+++ app/request.py
@@ -13,9 +13,10 @@ def get_quote():
     
     get_quote_url = base_url
 
-    with urllib.request.urlopen('http://quotes.stormconsultancy.co.uk/random.json' 'verify = True') as url:
+    with urllib.request.urlopen(get_quote_url) as url:
         get_quote_data = url.read()
         get_quote_response = json.loads(get_quote_data)
+        print(get_quote_response)
         
         author = get_quote_response.get('author')
         quote = get_quote_response.get('quote')
